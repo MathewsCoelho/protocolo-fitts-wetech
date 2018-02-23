baa15a558db0de17e4af015afeeb50d7e390e3a2
Krishna Xavier <tsixav@gmail.com>
2018-02-22 21:34:24 -0300

    Evento para carregar as variaveis de controle após o carregamento dos componentes do DOM, função para iniciar essas variaveis. Função de redução de codigo

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,72 @@
-var flag1 = false;
-var flag2 = false;
-var flag3 = false;
-var flag4 = false;
+/* iniciar o script após o carregamento da 'pagina' */
+window.addEventListener('DOMContentLoaded', init)
 
-var cliques;
+
+function init(){	
+	/* FAZER: comentar o que são essas flags*/
+	flag1 = false;
+	flag2 = false;
+	flag3 = false;
+	flag4 = false;
+	
+	cliques = 0;
+}
 
 function start(){
-	var cliques = 0;
-	document.getElementById("circulo1").style.background = "black";
-	document.getElementById("circulo2").style.background = "#C0C0C0";
-	document.getElementById("circulo3").style.background = "#C0C0C0";
-	document.getElementById("circulo4").style.background = "#C0C0C0";
+	cliques = 0;
+	gId("circulo1").style.background = "black";
+	gId("circulo2").style.background = "#C0C0C0";
+	gId("circulo3").style.background = "#C0C0C0";
+	gId("circulo4").style.background = "#C0C0C0";
 	flag1 = true;
 }
 
 function clique(){
 	cliques++;
 }
+
 function muda1(){
 	if(flag1 == true){
-		document.getElementById("circulo1").style.background = "#C0C0C0";
-		document.getElementById("circulo4").style.background = "black";
+		gId("circulo1").style.background = "#C0C0C0";
+		gId("circulo4").style.background = "black";
 		flag4 = true;
 		flag1 = false;
 	}
 }
+
 function muda2(){
 	if(flag2 == true){
-		document.getElementById("circulo2").style.background = "#C0C0C0";
-		document.getElementById("circulo3").style.background = "black";
+		gId("circulo2").style.background = "#C0C0C0";
+		gId("circulo3").style.background = "black";
 		flag3 = true;
 		flag2 = false;
 	}
 }
+
 function muda3(){
 	if(flag3 == true){
-		document.getElementById("circulo3").style.background = "#C0C0C0";
-		document.getElementById("circulo1").style.background = "black";
+		gId("circulo3").style.background = "#C0C0C0";
+		gId("circulo1").style.background = "black";
 		flag1 = true;
 		flag3 = false;
 	}
 }
+
 function muda4(){
 	if(flag4 == true){
-		document.getElementById("circulo4").style.background = "#C0C0C0";
-		document.getElementById("circulo2").style.background = "black";
+		gId("circulo4").style.background = "#C0C0C0";
+		gId("circulo2").style.background = "black";
 		flag2 = true;
 		flag4 = false;
 	}
+}
+
+/* funções de redução, para diminuir codigo */
+
+function gId(id){
+	return document.getElementById(id)
+}
+
+function c(mensagem){
+	console.log(mensagem)
 }
