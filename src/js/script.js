0568141b5f36efa7851fe6218e271313f77915f8
Krishna Xavier <tsixav@gmail.com>
2018-02-27 17:17:08 -0300

    Função de coordenadas dos elementos, não foi testada

--- src/js/script.js
+++ src/js/script.js
@@ -133,3 +133,63 @@ function c(mensagem){
 		console.log(mensagem)
 	}	
 }
+
+/* função de calculos matematicas */
+
+/* Adriano Ferreira, 2018 */
+function coordElements (quantidadeElementos, raio, lar, alt ) {
+	/* raio -> distancia entre o centro da tela e os circulos */
+	/* largura da tela/div */
+	/* altura da tela/div */
+	
+	let circunferencia = 360;
+	let angulo_inicial = circunferencia/quantidadeElementos;
+	let coeficiente = 0;
+	let resultado = [];
+
+	for (let i = 0, angulo = angulo_inicial; angulo<=360; angulo+=angulo_inicial, i++){
+		x = undefined;
+		y = undefined;
+		
+		if ( angulo == 90 ){
+			x = 0;
+			y = raio;
+		}
+		else if ( angulo == 270){
+			x = 0;
+			y = -raio;
+		} // -- casos em que o coeficiente angular(tangente) é infinito
+	
+		else {
+		(angulo == 180 || angulo == 360)?coeficiente = 0: coeficiente = Math.tan( angulo * Math.PI/180 ); // Casos em que é 0 
+		}
+		
+		if ( typeof x == 'undefined'){
+			x = Math.pow(raio,2)/(Math.pow(coeficiente,2)+1); //
+			x = Math.sqrt(x);
+			y = coeficiente*x;
+		}
+					
+		/* Devido ao fato de que inverte-se os lados */
+		if (angulo>90 && angulo<270){
+			x *= -1;
+			y *= -1;
+		}
+	
+		//------ fim do sistema
+		d = Math.pow(x,2) + Math.pow(y,2); // distancia considerando ponto inicial (0,0);
+		d = Math.sqrt(d);
+		c("Angulo: "+angulo+" x: " + x + " y: "+y);
+		c("Coeficiente: "+coeficiente);
+		c("distancia: "+d); // valor do inicial ate o angulo de 90, adaptar para o circulo
+	 
+		x+=lar; // Adaptando para pontos nao nulos
+		y+=alt;
+
+		resultado[i] = {}
+		resultado[i]['x'] = x;
+		resultado[i]['y'] = y;
+	
+	}
+	return resultado;
+}
